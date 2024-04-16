2e199efd1bb64816162b2f197665130b90a6dbb9
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-04-16 19:51:30 -0300

    refactoring cpf function and tests coverage

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,35 @@
 // @ts-nocheck
-export function validate (str) {
 
-	if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 && str.length <= 14){
+export function validateCpf(rawCpf) {
+    if (!rawCpf) return false;
+    const cleanCpf = rawCpf.replace(/\D/g,"");
+    if (isInvalidLength(cleanCpf)) return false;
+    if (allDigitsTheSame(cleanCpf)) return false;
+    const digit1 = calculateDigit(cleanCpf, 10);
+    const digit2 = calculateDigit(cleanCpf, 11);
+    const actualDigit = extractsDigits(cleanCpf);
+    const validatedDigit = `${digit1}${digit2}`
+    return actualDigit === validatedDigit;
+}
 
-                str=str
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");  
-    
-                if (!str.split("").every(c => c === str[0])) {
-                    try{  
-                        let     d1, d2;  
-                        let     dg1, dg2, rest;  
-                        let     digito;  
-                            let     nDigResult;  
-                        d1 = d2 = 0;  
-                        dg1 = dg2 = rest = 0;  
-                            
-                        for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                            // 	return false;
-                            // } else {
-    
-                                digito = parseInt(str.substring(nCount -1, nCount));  							
-                                d1 = d1 + ( 11 - nCount ) * digito;  
-                
-                                d2 = d2 + ( 12 - nCount ) * digito;  
-                            // }
-                        };  
-                            
-                        rest = (d1 % 11);  
-                
-                        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-                        d2 += 2 * dg1;  
-                        rest = (d2 % 11);  
-                        if (rest < 2)  
-                            dg2 = 0;  
-                        else  
-                            dg2 = 11 - rest;  
-                
-                            let nDigVerific = str.substring(str.length-2, str.length);  
-                        nDigResult = "" + dg1 + "" + dg2;  
-                        return nDigVerific == nDigResult;
-                    }catch (e){  
-                        console.error("Erro !"+e);  
-    
-                        return false;  
-                    }  
-                } else return false
-    
-            }else return false;
-        }
+function calculateDigit(cpf: string, factor: number) {
+    let total = 0;
+    for(const digit of cpf) {
+        if(factor > 1) total += digit * factor--;
+    }
+    const rest = total%11;
+    return (rest < 2) ? 0 : 11 - rest;
+}
 
+function isInvalidLength(cpf: string) {
+    return cpf.length !== 11
+}
 
-	} else return false;
+function allDigitsTheSame(cpf: string) {
+    const [firstDigit] = cpf;
+    return [...cpf].every(digit => digit === firstDigit);
+}
 
+function extractsDigits(cpf: string) {
+    return cpf.slice(9);
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,4 +1,4 @@
-import { validate } from "../src/main";
+import { validateCpf } from "../src/main";
 
 const validCpfs = [
     '987.654.321-00',
@@ -8,32 +8,38 @@ const validCpfs = [
 ]
 
 test.each(validCpfs)('Deve testar um cpf valido: %s', function (cpf: string) {
-    const isValid = validate(cpf);
+    const isValid = validateCpf(cpf);
     expect(isValid).toBeTruthy();
 })
 
 const invalidCpfs = [
     '111.111.111-11',
     '222.222.222-22',
-    '333.333.333-33'
+    '333.333.333-33',
+    '444.444.444-44',
+    '555.555.555-55',
+    '666.666.666-66',
+    '777.777.777-77',
+    '888.888.888-88',
+    '999.999.999-99',
 ]
 
 test.each(invalidCpfs)('Deve testar um cpf invalido: %s', function (cpf: string) {
-    const isValid = validate(cpf);
+    const isValid = validateCpf(cpf);
     expect(isValid).toBeFalsy();
 })
 
 test('Deve testar um cpf undefined', function() {
-    const isValid = validate(undefined);
+    const isValid = validateCpf(undefined);
     expect(isValid).toBeFalsy();
 })
 
 test('Deve testar um cpf null', function() {
-    const isValid = validate(null);
+    const isValid = validateCpf(null);
     expect(isValid).toBeFalsy();
 })
 
 test('Deve testar um cpf com tamanho errado', function() {
-    const isValid = validate('123');
+    const isValid = validateCpf('123');
     expect(isValid).toBeFalsy();
 })
