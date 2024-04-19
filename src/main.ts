1cffebde5c9063506878eda80b9550345da9481b
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-04-18 21:24:13 -0300

    add database queries

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,17 @@ app.use(express.json());
 const connection = pgp()('postgres://app:app@localhost:5432/eccommerce_app');
 if (connection) console.log('Database Postgres connected')
 
-const products = [
-    { idProduct: 1, description: "A", price: 1000 },
-    { idProduct: 2, description: "B", price: 5000 },
-    { idProduct: 3, description: "C", price: 30 },
-];
+// const products = [
+//     { idProduct: 1, description: "A", price: 1000 },
+//     { idProduct: 2, description: "B", price: 5000 },
+//     { idProduct: 3, description: "C", price: 30 },
+// ];
 
-const coupons = [
-    { code: "VALE20", percentage: 20 }
-];
+// const coupons = [
+//     { code: "VALE20", percentage: 20 }
+// ];
 
-app.post('/checkout', function (req, res) {
+app.post('/checkout', async function (req, res) {
     const isValid = validate(req.body.cpf);
     if (!isValid) {
         return res.status(422).json({
@@ -27,7 +27,8 @@ app.post('/checkout', function (req, res) {
 
     let total = 0;
     for (const item of req.body.items) {
-        const product = products.find((product) => product.idProduct === item.idProduct);
+        // const product = products.find((product) => product.idProduct === item.idProduct);
+        const [product] = await connection.query('select * from eccommerce_app.product where id_product = $1', [item.id_product]);
         if (product) {
             total += product.price * item.quantity;
         } else {
@@ -38,7 +39,8 @@ app.post('/checkout', function (req, res) {
     };
 
     if (req.body.coupon) {
-        const coupon = coupons.find((coupon) => coupon.code === req.body.coupon);
+        // const coupon = coupons.find((coupon) => coupon.code === req.body.coupon);
+        const [coupon] = await connection.query('select * from eccommerce_app.coupons where code = $1',[req.body.coupon])
         if (coupon) {
             total -= (total * coupon?.percentage) / 100;
         }
