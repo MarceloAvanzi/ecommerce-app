dc35594e6ccb70b27823fcf6a410f127c631b52b
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-05-27 20:22:57 -0300

    cli with observer pattern

--- src/infrastructure/cli/CLIController.ts
+++ src/infrastructure/cli/CLIController.ts
@@ -4,11 +4,23 @@ import CLIHandler from "./CLIHandler";
 export default class CLIController {
     constructor(readonly handler: CLIHandler, readonly checkout: Checkout) {
         const input: any = {
-            itens: []
+            items: []
         }
-        handler.on('set-cpf', function (params: string){
+
+        handler.on('set-cpf', function (params: string) {
             input.cpf = params;
             console.log(input);
+        })
+
+        handler.on('add-item', function (params: string) {
+            const [idProduct, quantity] = params.split(' ');
+            input.items.push({ idProduct: parseInt(idProduct), quantity: parseInt(quantity) });
+            console.log(input);
+        })
+
+        handler.on('checkout', async function (params: string) {
+            const output = await checkout.execute(input);
+            console.log(output);
         })
     }
 }
