ce19da08b2b1d7779df13f179f5d1e7ca23b057f
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-06-03 19:58:12 -0300

    configuring vue frontend with tests

---
+++ frontend/test/App.test.ts
@@ -0,0 +1,16 @@
+import { mount } from '@vue/test-utils'
+import AppVue from '../src/App.vue'
+test('Deve testar o frontend', async function () {
+    const wrapper = mount(AppVue, {
+    });
+    expect(wrapper.get('.title').text()).toBe('Checkout');
+    expect(wrapper.findAll('.product-description').at(0)?.text()).toBe('A');
+    expect(wrapper.findAll('.product-price').at(0)?.text()).toBe('1000');
+    expect(wrapper.findAll('.product-description').at(1)?.text()).toBe('B');
+    expect(wrapper.findAll('.product-price').at(1)?.text()).toBe('5000');
+    expect(wrapper.findAll('.product-description').at(2)?.text()).toBe('C');
+    expect(wrapper.findAll('.product-price').at(2)?.text()).toBe('30');
+
+    await wrapper.findAll('.product-add-button').at(0)?.trigger('click');
+    expect(wrapper.get('.total').text()).toBe('1000');
+})
