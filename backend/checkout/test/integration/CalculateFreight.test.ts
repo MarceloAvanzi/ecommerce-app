532ec07bd76bfe152514c7117feaa6d3d94a5c84
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-06-06 19:36:21 -0300

    adding real freight calculator

--- backend/checkout/test/integration/CalculateFreight.test.ts
+++ backend/checkout/test/integration/CalculateFreight.test.ts
@@ -1,16 +1,16 @@
 import ProductDataDatabase from "../../src/infrastructure/data/ProductDataDatabase";
-import SimulateFreight from "../../src/application/SimulateFreight";
+import CalculateFreight from "../../src/application/CalculateFreight";
 import PgPromiseConnection from "../../src/infrastructure/database/PgPromiseConnection";
 
 test('Deve simular o frete para um pedido', async function () {
     const connection = new PgPromiseConnection();
     const productData = new ProductDataDatabase(connection);
-    const simulateFreight = new SimulateFreight(productData);
+    const calculateFreight = new CalculateFreight(productData);
     const input = {
         items: [
             {idProduct: 1, quantity: 1}
         ]
     };
-    const output = await simulateFreight.execute(input);
+    const output = await calculateFreight.execute(input);
     expect(output.total).toBe(30);
 })
--- backend/checkout/test/unit/FreightCalculator.test.ts
+++ backend/checkout/test/unit/FreightCalculator.test.ts
@@ -1,7 +1,7 @@
 import FreightCalculator from "../../src/domain/entities/FreightCalculator"
 import Product from "../../src/domain/entities/Product"
 
-test('Deve calcular o frete', function () {
+test('Deve calcular o frete com distancia padrao', function () {
     const product = new Product(1, 'A', 1000, 100, 30, 10, 3);
     const freight = FreightCalculator.calculate(product)
     expect(freight).toBe(30)
@@ -11,4 +11,11 @@ test('Deve calcular o frete minimo', function () {
     const product = new Product(3, 'C', 10, 10, 10, 10, 0.9)
     const freight = FreightCalculator.calculate(product)
     expect(freight).toBe(10)
+})
+
+test('Deve calcular o frete com distancia variavel', function () {
+    const product = new Product(1, 'A', 1000, 100, 30, 10, 3);
+    const distance = 748.2217780081631;
+    const freight = FreightCalculator.calculate(product, distance)
+    expect(freight).toBe(22.45)
 })
