c6c5b66b04762907e359f802e5e8c19f8b4365cd
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-06-10 21:05:56 -0300

    split freight into subdomain

---
+++ backend/freight/src/main_api.ts
@@ -0,0 +1,20 @@
+import Checkout from "./application/Checkout";
+import ProductDataDatabase from "./infrastructure/data/ProductDataDatabase";
+import CouponDataDatabase from "./infrastructure/data/CouponDataDatabase";
+import OrderDataDatabase from "./infrastructure/data/OrderDataDatabase";
+import PgPromiseConnection from "./infrastructure/database/PgPromiseConnection";
+import ExpressHttpServer from "./infrastructure/http/ExpressHttpServer";
+import RestController from "./infrastructure/controller/RestController";
+import ZipcodeDataDatabase from "./infrastructure/data/ZipcodeDataDatabase";
+import CalculateFreight from "./application/CalculateFreight";
+
+const connection = new PgPromiseConnection();
+const httpServer = new ExpressHttpServer();
+const productData = new ProductDataDatabase(connection);
+const couponData = new CouponDataDatabase(connection);
+const orderData = new OrderDataDatabase(connection);
+const zipcodeData = new ZipcodeDataDatabase(connection);
+const calculateFreight = new CalculateFreight(productData, zipcodeData);
+const checkout = new Checkout(productData, couponData, orderData, calculateFreight);
+new RestController(httpServer, checkout);
+httpServer.listen(3000);
