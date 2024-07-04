ea794eca8b9626f75b5eda3c9b1ec581b07cab94
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-07-04 20:19:45 -0300

    finish stock

--- backend/stock/src/main_api.ts
+++ backend/stock/src/main_api.ts
@@ -1,8 +1,18 @@
 import PgPromiseConnection from "./infrastructure/database/PgPromiseConnection";
 import ExpressHttpServer from "./infrastructure/http/ExpressHttpServer";
 import RestController from "./infrastructure/controller/RestController";
+import StockEntryRepositoryDatabase from "./infrastructure/repository/StockEntryRepositoryDatabase";
+import IncreaseStock from "./application/IncreaseStock";
+import DecreaseStock from "./application/DecreaseStock";
+import CalculateStock from "./application/CalculateStock";
+import CleanStock from "./application/CleanStock";
 
 const connection = new PgPromiseConnection();
 const httpServer = new ExpressHttpServer();
-new RestController(httpServer);
+const stockEntryRepository = new StockEntryRepositoryDatabase(connection);
+const calculateStock = new CalculateStock(stockEntryRepository);
+const increaseStock = new IncreaseStock(stockEntryRepository);
+const decreaseStock = new DecreaseStock(stockEntryRepository);
+const cleanStock = new CleanStock(stockEntryRepository);
+new RestController(httpServer, calculateStock, increaseStock, decreaseStock, cleanStock);
 httpServer.listen(3003)
